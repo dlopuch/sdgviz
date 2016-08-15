1e9dbce8b56ae71897ddae266eca3bf301a2fbbe
Dan Lopuch <dan@datahero.com>
2016-08-14 23:08:45 -0700

    Activiation react minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,34 @@
+/**
+ * Normal build creates an unminified bundle with sourcemap.
+ *
+ * To make a production build, run webpack as:
+ *   NODE_ENV=production webpack --optimize-minimize --devtool none
+ */
 
 const APP_ROOT = __dirname + '/public/js';
+const webpack = require('webpack');
+
+const PLUGINS = [];
+
+/**
+ * React Optimization with Build Script Activation.
+ * ------
+ * React has a lot of developer-mode code that slows stuff down but gives helpful errors.  This code can be built in
+ * "production mode" by treating this as dead code and getting it minified away.  It will be recognized as dead code by
+ * the minifier if process.env.NODE_ENV is set to 'production' using the DefinePlugin.
+ * However, React's dead-code process.env.NODE_ENV is different from running Webpack with NODE_ENV=production.
+ * Thus, our *build script* turns on *React* minification (via DefinePlugin) if the same NODE_ENV convention is set.
+ */
+if (process.env.NODE_ENV === 'production') {
+  PLUGINS.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  );
+}
+
 
 module.exports = {
   context: APP_ROOT,
@@ -26,6 +55,7 @@ module.exports = {
       { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
     ]
   },
+  plugins: PLUGINS,
   resolve: {
     root: APP_ROOT
   },
