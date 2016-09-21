95a22a1dd52d132482cf8fdb298c5135aac03287
Dan Lopuch <dan@datahero.com>
2016-09-21 12:31:30 -0700

    Layout

---
+++ public/js/react/ChartContainer.jsx
@@ -0,0 +1,24 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const ChartBaseView = require('../charts/ChartBaseView');
+
+module.exports = React.createClass({
+  mixins: [],
+
+  shouldComponentUpdate() {
+    // Let D3 scripts and bootstrap plugins manage the dom
+    return false;
+  },
+
+  componentDidMount() {
+    window.chartBaseView = new ChartBaseView(ReactDOM.findDOMNode(this.refs.sdgvSvg))
+  },
+
+  render() {
+    return (
+      <div className="sdgv-svg-container">
+        <svg id="sdgv-svg" className="sdgv-svg" ref="sdgvSvg" width="150" height="300"></svg>
+      </div>
+    );
+  }
+});
--- public/js/react/reactApp.jsx
+++ public/js/react/reactApp.jsx
@@ -3,20 +3,22 @@ const ReactDOM = require('react-dom');
 
 const actions = require('../actions');
 
+const ChartContainer = require('./ChartContainer.jsx');
 const DisplayCrossfilterData = require('./DisplayCrossfilterData.jsx');
 
 module.exports = function reactApp() {
   ReactDOM.render(
-    <div className="container-fluid">
-      <div>
-        <div className="sdgviz-select-drilldown">
-          Select view:
-          <button onClick={actions.selectDrilldown.allAmount}>All Pledges</button>
-          <button onClick={actions.selectDrilldown.amountBySdg}>By SDG</button>
-          <button onClick={actions.selectDrilldown.amountByOrg}>By Organization</button>
-        </div>
-        <DisplayCrossfilterData></DisplayCrossfilterData>
+    <div className="sdgv container-fluid">
+      <h3>GSC Half-Billion Challenge Progress</h3>
+      <div className="sdgviz-select-drilldown">
+        Select view:
+        <button onClick={actions.selectDrilldown.allAmount}>All Pledges</button>
+        <button onClick={actions.selectDrilldown.amountBySdg}>By SDG</button>
+        <button onClick={actions.selectDrilldown.amountByOrg}>By Organization</button>
       </div>
+
+      <ChartContainer></ChartContainer>
+      <DisplayCrossfilterData></DisplayCrossfilterData>
     </div>,
     document.getElementById('react-app')
   );
