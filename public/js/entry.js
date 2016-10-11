4f8245ad5db48b7ed4fa45d11b89fc193cfd24a1
Dan Lopuch <dan@datahero.com>
2016-10-11 10:52:15 -0700

    Removing debugging interfaces

--- public/js/entry.js
+++ public/js/entry.js
@@ -1,9 +1,9 @@
 const reactApp = require('./react/reactApp.jsx');
-window.d3 = require('d3');
 
 // Load d3 plugins:
 // TODO: Doesn't work.  The plugin extends module's own dependency d3-selection, not above.
 // Webpack/npm issue?
+// window._d3 = require('d3');
 // require('d3-selection-multi');
 
 require('./style.less');
@@ -11,12 +11,9 @@ require('./style.less');
 const promiseData = require('./promiseData');
 const actions = require('./actions');
 
-window.actions = actions;
-
 window.onload = function onload() {
   promiseData.then(data => {
-    console.log('GOT DATA!', data);
-    window.hdData = data;
+    window._sdgChallengeData = data;
   });
 
   reactApp();
